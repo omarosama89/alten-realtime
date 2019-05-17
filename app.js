fa0ab581ab455078d2498de07c7d188e9426f84d
Omar <o.mohammed@morniksa.com>
2019-05-17 04:30:13 +0200

    change port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-server.listen(portName, function () {
+server.listen(process.env.PORT || 4000, function () {
     console.log(`listen to port ${portName}`)
 });
 app.post('/notify', function (req, res) {
