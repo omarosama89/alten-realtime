bd4b47bbbf7b32d887c63da150f322105c4e63c9
Omar <o.mohammed@morniksa.com>
2019-09-28 17:03:17 +0200

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let app = require('express')();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let bodyParser = require('body-parser');
-let portName = 4000;
+let portName = 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -11,18 +11,15 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-server.listen(process.env.PORT || 4000, function () {
+server.listen(process.env.PORT || portName, function () {
     console.log(`listen to port ${portName}`)
 });
 app.post('/notify', function (req, res) {
-    let channel = 'vehicle_notifier';
-    let body = req.body;
-    io.sockets.emit(`${channel}`, body);
-    res.send('What is up ');
+	io.sockets.emit(req.body.channel_name, req.body.object )
+        console.log(req.body.channel_name)
+        res.send({message: 'hello there'});
+
 });
 app.get('/test', function(req, res){
-    let channel = 'vehicle_notifier';
-    let body = req.body;
-    io.sockets.emit(`${channel}`, {vehicle: {id: '5'}});
-    res.send('What is up ');
+    res.send('Realtimeserver is up and running.');
 });
